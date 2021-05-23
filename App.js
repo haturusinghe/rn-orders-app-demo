4f5be2c65a61f0b7720b9bc3bc3b9b4db188a08a
S.Haturusinghe <haturusingh3@gmail.com>
2021-05-23 07:44:10 +0530

    deleted old files

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
-import React from 'react';
-
-import Providers from './navigation';
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import RegistrationScreen from "./screens/RegistrationScreen";
 
 export default function App() {
-  return <Providers />;
+  return <RegistrationScreen />;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
